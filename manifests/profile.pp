20c60981ba070acd6a7e7768b12aa68a0a4489cb
Gildas Cherruel <gildas.cherruel@inin.com>
2014-02-25 13:57:41 +0900

    Added new resource: windows firewall profile

---
+++ manifests/profile.pp
@@ -0,0 +1,34 @@
+define firewall::profile(
+  $ensure,
+  $profile = $name,
+  $display = undef,
+)
+{
+  if (!$::operatingsystem != 'Windows')
+  {
+    err("Firewall groups are not supported on ${::operatingsystem}")
+    fail("Unsupported OS: ${::operatingsystem}")
+  }
+
+  $display_option = empty($display) ? { true => '', default => "-DisplayGroup \"${display}\"" }
+  case $ensure
+  {
+    present, enabled:
+    {
+      exec {"Enable-Firewall-Profile-${profile}":
+        command  => "Set-NetFirewallProfile -Profile ${profile} -Enabled True",
+        onlyif   => "if ((Get-NetFirewallProfile -Profile ${profile}).Enabled) { exit 1 }",
+        provider => powershell,
+      }
+    }
+    absent, disabled:
+    {
+      exec {"Disable-Firewall-Profile-${profile}":
+        command  => "Set-NetFirewallProfile -Profile ${profile} -Enabled False",
+        onlyif   => "if (!(Get-NetFirewallProfile -Profile ${profile}).Enabled) { exit 1 }",
+        provider => powershell,
+      }
+    }
+    default: { fail("Unsupported ensure: ${ensure}") }
+  }
+}
