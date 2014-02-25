7c14f9126bb964adf879bd8a75fa8850efd512e9
Gildas Cherruel <gildas.cherruel@inin.com>
2014-02-25 13:49:45 +0900

    Added default hiera configuration

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,5 +20,39 @@
 #
 class firewall {
 
+  # Enable/Disable firewall profiles via hiera
+  $profiles = hiera_array('firewall::profiles', [])
+  if (!empty($profiles))
+  {
+    notice(" Checking firewall profiles: ${profiles}")
+    $profile_default = {
+      ensure => enabled,
+    }
 
+    create_resources(firewall::profile, $profiles, $profile_default)
+  }
+
+  # Enable/Disable firewall groups via hiera
+  $groups = hiera_array('firewall::groups', [])
+  if (!empty($groups))
+  {
+    notice(" Checking firewall groups: ${groups}")
+    $group_default = {
+      ensure => enabled,
+    }
+
+    create_resources(firewall::group, $groups, $group_default)
+  }
+
+  # Enable/Disable firewall rules via hiera
+  $rules = hiera_array('firewall::rules', [])
+  if (!empty($rules))
+  {
+    notice(" Checking firewall rules: ${rules}")
+    $rule_default = {
+      ensure => enabled,
+    }
+
+    create_resources(firewall::rule, $rules, $rule_default)
+  }
 }
