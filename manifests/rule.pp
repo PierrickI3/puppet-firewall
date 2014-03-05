800f599467f7124d1a5c9e1563a4aa42fc9c376e
Gildas Cherruel <gildas.cherruel@inin.com>
2014-03-05 16:37:26 +0900

    Started to add support for Windows 7 and 2008R2

--- manifests/rule.pp
+++ manifests/rule.pp
@@ -10,25 +10,54 @@ define firewall::rule(
     fail("Unsupported OS: ${::operatingsystem}")
   }
 
-  $display_option = empty($display) ? { true => '', default => "-DisplayName \"${display}\"" }
-  case $ensure
+  #TODO: Use profiles!
+  case $::operatingsystemrelease
   {
-    present, enabled:
+    '6.1.7601' : # Windows 7, 2008R2
     {
-      exec {"Enable-Firewall-Rule-${name}":
-        command  => "Enable-NetFirewallRule ${display_option} -Name \"${rule}\"",
-        onlyif   => "if ((Get-NetFirewallRule ${display_option} -Name \"${rule}\" -ErrorAction Ignore).Enabled) { exit 1 }",
-        provider => powershell,
+      $group_id=downcase($group)
+      case $ensure
+      {
+        present, enabled:
+        {
+          exec {"Enable-Firewall-Rule-${name}":
+            command  => "netsh advfirewall firewall set rule name=\"${rule}\"",
+            onlyif   => "if ((netsh advfirewall show rule name=\"${rule}\") | where {\$ -match '^Enabled\s+Yes'} ) { exit 1 }",
+            provider => powershell,
+          }
+        }
+        absent, disabled:
+        {
+          #TODO: Code this
+          err("Not coded yet")
+          fail("Not coded yet")
+        }
+        default: { fail("Unsupported ensure: ${ensure}") }
       }
     }
-    absent, disabled:
+    default:      # Windows 8, 8.1, 2012, 2012R2
     {
-      exec {"Disable-Firewall-Rule-${name}":
-        command  => "Disable-NetFirewallRule ${display_option} -Name \"${rule}\"",
-        onlyif   => "if (!(Get-NetFirewallRule -name \"${rule}\" -ErrorAction Ignore).Enabled) { exit 1 }",
-        provider => powershell,
+      $display_option = empty($display) ? { true => '', default => "-DisplayName \"${display}\"" }
+      case $ensure
+      {
+        present, enabled:
+        {
+          exec {"Enable-Firewall-Rule-${name}":
+            command  => "Enable-NetFirewallRule ${display_option} -Name \"${rule}\"",
+            onlyif   => "if ((Get-NetFirewallRule ${display_option} -Name \"${rule}\" -ErrorAction Ignore).Enabled) { exit 1 }",
+            provider => powershell,
+          }
+        }
+        absent, disabled:
+        {
+          exec {"Disable-Firewall-Rule-${name}":
+            command  => "Disable-NetFirewallRule ${display_option} -Name \"${rule}\"",
+            onlyif   => "if (!(Get-NetFirewallRule -name \"${rule}\" -ErrorAction Ignore).Enabled) { exit 1 }",
+            provider => powershell,
+          }
+        }
+        default: { fail("Unsupported ensure: ${ensure}") }
       }
     }
-    default: { fail("Unsupported ensure: ${ensure}") }
   }
 }
