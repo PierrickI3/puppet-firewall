6cc0c468645b5acee966c0c16728a53b39965c75
Gildas Cherruel <gildas@breizh.org>
2014-03-06 08:02:53 +0900

    Added support for Windows 7 and 2008R2 in profiles

--- manifests/profile.pp
+++ manifests/profile.pp
@@ -1,7 +1,6 @@
 define firewall::profile(
   $ensure,
   $profile = $name,
-  $display = undef,
 )
 {
   if ($::operatingsystem != 'windows')
@@ -10,25 +9,54 @@ define firewall::profile(
     fail("Unsupported OS: ${::operatingsystem}")
   }
 
-  $display_option = empty($display) ? { true => '', default => "-DisplayGroup \"${display}\"" }
-  case $ensure
+  case $::operatingsystemrelease
   {
-    present, enabled:
+    '6.1.7601' : # Windows 7, 2008R2
     {
-      exec {"Enable-Firewall-Profile-${name}":
-        command  => "Set-NetFirewallProfile -Profile \"${profile}\" -Enabled True",
-        onlyif   => "if ((Get-NetFirewallProfile -Profile \"${profile}\").Enabled) { exit 1 }",
-        provider => powershell,
+      $profile_id=downcase($profile)
+      case $ensure
+      {
+        present, enabled:
+        {
+          exec {"Enable-Firewall-Profile-${name}":
+            command  => "netsh advfirewall set ${profile_id}profile state on",
+            onlyif   => "if ((netsh advfirewall show ${profile_id}profile state) | where {\$ -match '^State\s+ON'} ) { exit 1 }",
+            provider => powershell,
+          }
+        }
+        absent, disabled:
+        {
+          exec {"Disable-Firewall-Profile-${name}":
+            command  => "netsh advfirewall set ${profile_id}profile state off",
+            onlyif   => "if ((netsh advfirewall show ${profile_id}profile state) | where {\$ -match '^State\s+OFF'} ) { exit 1 }",
+            provider => powershell,
+          }
+        }
+        default: { fail("Unsupported ensure: ${ensure}") }
       }
     }
-    absent, disabled:
+    default:      # Windows 8, 8.1, 2012, 2012R2
     {
-      exec {"Disable-Firewall-Profile-${name}":
-        command  => "Set-NetFirewallProfile -Profile \"${profile}\" -Enabled False",
-        onlyif   => "if (!(Get-NetFirewallProfile -Profile \"${profile}\").Enabled) { exit 1 }",
-        provider => powershell,
+      case $ensure
+      {
+        present, enabled:
+        {
+          exec {"Enable-Firewall-Profile-${name}":
+            command  => "Set-NetFirewallProfile -Profile \"${profile}\" -Enabled True",
+            onlyif   => "if ((Get-NetFirewallProfile -Profile \"${profile}\").Enabled) { exit 1 }",
+            provider => powershell,
+          }
+        }
+        absent, disabled:
+        {
+          exec {"Disable-Firewall-Profile-${name}":
+            command  => "Set-NetFirewallProfile -Profile \"${profile}\" -Enabled False",
+            onlyif   => "if (!(Get-NetFirewallProfile -Profile \"${profile}\").Enabled) { exit 1 }",
+            provider => powershell,
+          }
+        }
+        default: { fail("Unsupported ensure: ${ensure}") }
       }
     }
-    default: { fail("Unsupported ensure: ${ensure}") }
   }
 }
